7e82fc8678de52f4fb6f1c09b9cabd805240ec0a
wagaskarabhi <abhiwagaskar6383@gmail.com>
2024-03-07 15:44:01 +0530

    first commit

---
+++ src/Components/Todoitems.jsx
@@ -0,0 +1,53 @@
+import "./CSS/Todoitems.css";
+import tick from "./Assets/tick.png";
+import not_tick from "./Assets/not_tick.png";
+import remove from "./Assets/remove.png";
+
+const Todoitems = ({ no, display, text, setTodos }) => {
+  const deleteTodo = (no) => {
+    let data = JSON.parse(localStorage.getItem("todos"));
+    data = data.filter((todo) => todo.no !== no);
+    setTodos(data);
+  };
+  const toggle = (no) => {
+    let data = JSON.parse(localStorage.getItem("todos"));
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].no === no) {
+        if (data[i].display === "") {
+          data[i].display = "line-through";
+        } else {
+          data[i].display = "";
+        }
+        break;
+      }
+    }
+    setTodos(data);
+  };
+  return (
+    <div className="todoitems">
+      <div
+        className={`todoitems-container ${display}`}
+        onClick={() => {
+          toggle(no);
+        }}
+      >
+        {display === "" ? (
+          <img className="tick-not" src={not_tick} alt=""></img>
+        ) : (
+          <img className="tick-not" src={tick} alt=""></img>
+        )}
+        <div className="todoitems-text">{text}</div>
+      </div>
+      <img
+        className="todoitem-cross-icon"
+        onClick={() => {
+          deleteTodo(no);
+        }}
+        src={remove}
+        alt=""
+      ></img>
+    </div>
+  );
+};
+
+export default Todoitems;
